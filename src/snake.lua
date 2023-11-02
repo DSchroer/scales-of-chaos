d976815fbda511cef51a3b41c08538102d9ff204
Dominick Schroer <dominick@schroer.ca>
2023-11-02 08:48:59 -0600

    add rotation

--- src/snake.lua
+++ src/snake.lua
@@ -2,14 +2,22 @@ snake = { x = 100, y = 100, dir = 0, speed = 300, turn_speed = 3, tail_length = 
 
 snake.tail = {}
 for i=1,snake.tail_length do
-    snake.tail[i] = { x = snake.x - (snake.tail_distance * i), y = snake.y }
+    snake.tail[i] = { x = snake.x - (snake.tail_distance * i), y = snake.y, dir = 0 }
 end
 
 function snake.draw()
-    love.graphics.circle("line", torus_x(snake.x), torus_y(snake.y), 25, 10)
+    love.graphics.push()
+    love.graphics.translate(torus_x(snake.x), torus_y(snake.y))
+    love.graphics.rotate(-snake.dir)
+    love.graphics.circle("line", 0, 0, 25, 10)
+    love.graphics.pop()
 
     for i=1,#snake.tail do
-        love.graphics.circle("line", torus_x(snake.tail[i].x), torus_y(snake.tail[i].y), 25, 10)
+        love.graphics.push()
+        love.graphics.translate(torus_x(snake.tail[i].x), torus_y(snake.tail[i].y))
+        love.graphics.rotate(-snake.tail[i].dir)
+        love.graphics.circle("line", 0, 0, 25, 6)
+        love.graphics.pop()
     end
 end
 
@@ -30,11 +38,11 @@ function snake.update(dt)
  
     for i=1,#snake.tail do
         dist = distance(tx, ty, snake.tail[i].x, snake.tail[i].y) - snake.tail_distance
+        dir = math.atan2(tx - snake.tail[i].x, ty - snake.tail[i].y)
 
-        angle = math.atan2(tx - snake.tail[i].x, ty - snake.tail[i].y)
-
-        snake.tail[i].x = snake.tail[i].x + (dist * math.sin(angle))
-        snake.tail[i].y = snake.tail[i].y + (dist * math.cos(angle))
+        snake.tail[i].dir = dir
+        snake.tail[i].x = snake.tail[i].x + (dist * math.sin(dir))
+        snake.tail[i].y = snake.tail[i].y + (dist * math.cos(dir))
 
         tx = snake.tail[i].x
         ty = snake.tail[i].y
