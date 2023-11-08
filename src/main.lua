6e7f88e22273a6ea1d96ecaef39bb4a6adfe2966
Dominick Schroer <dominick@schroer.ca>
2023-11-08 11:12:17 -0700

    background

--- src/main.lua
+++ src/main.lua
@@ -5,12 +5,19 @@ enemies:spawn()
 enemies:spawn()
 enemies:spawn()
 
+function love.load()
+    bg = love.graphics.newImage("assets/background.jpg")
+end
+
 function love.update(dt)
     enemies:update(dt)
     snake:update(dt)
 end
 
 function love.draw()
+    love.graphics.reset()
+    love.graphics.draw(bg, 0, 0, 0, 2, 2)
+
     enemies:draw()
     snake:draw()
 end
